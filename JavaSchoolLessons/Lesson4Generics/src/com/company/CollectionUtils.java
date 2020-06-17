38a3a33c937cb2980e98601805bc6fa6a13be5bb
Mikhail Bolotov <bolotovmu@mail.ru>
2020-06-17 23:08:49 +0300

    Задания лекции 4 Generics часть 2 CollectionUtil

--- JavaSchoolLessons/Lesson4Generics/src/com/company/CollectionUtils.java
+++ JavaSchoolLessons/Lesson4Generics/src/com/company/CollectionUtils.java
@@ -8,52 +8,70 @@ import java.util.*;
 4. Параметризовать методы, используя правило PECS, и реализовать их
 */
 public class CollectionUtils {
-    public static<T> void addAll(List<? extends T> source, List<? super T> destination) {
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
     }
 
-    public static List newArrayList() {
-        throw new NotImplementedException();
+    public static <T> List<T> newArrayList() {
+        return new ArrayList<>();
     }
 
-    public static int indexOf(List source, Object o) {
-        throw new NotImplementedException();
+    public <T> int indexOf(List<? extends T> source, T o) {
+        return source.indexOf(o);
     }
 
-    public static List limit(List source, int size) {
-        throw new NotImplementedException();
+    public static <T> List<T> limit(List<T> source, int size) {
+        return source.subList(0, size - 1);
     }
 
-    public static void add(List source, Object o) {
-        throw new NotImplementedException();
+    public static <T> void add(List<? super T> source, T o) {
+        source.add(o);
     }
 
-    public static void removeAll(List removeFrom, List c2) {
-        throw new NotImplementedException();
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
+        removeFrom.removeAll(c2);
     }
 
     //true если первый лист содержит все элементы второго
-    public static boolean containsAll(List c1, List c2) {
-        throw new NotImplementedException();
+    public static <T> boolean containsAll(List<? super T> c1, List<? extends T> c2) {
+        return c1.containsAll(c2);
     }
 
     //true если первый лист содержит хотя-бы 1 второго
-    public static boolean containsAny(List c1, List c2) {
-        throw new NotImplementedException();
+    public static <T> boolean containsAny(List<? super T> c1, List<? extends T> c2) {
+        for (T o: c2) {
+            if (c1.contains(o)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    //Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
+    // Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
     // Элементы сравнивать через Comparable.
-    // Прмер range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
-    public static List range(List list, Object min, Object max) {
-        throw new NotImplementedException();
+    // Пример range(Arrays.asList(8,1,3,5,6,4), 3, 6) вернет {3,4,5,6}
+    public static <T> List<T> range(List<? extends T> list, T min, T max) {
+        List<T> res = newArrayList();
+        for (T o: list) {
+            Comparable<T> oComp = (Comparable<T>)o;
+            if (oComp.compareTo(min) >= 0 && oComp.compareTo(max) <= 0) {
+                res.add(o);
+            }
+        }
+        return res;
     }
 
-    //Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
-    // Элементы сравнивать через Comparable.
+    // Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
+    // Элементы сравнивать через Comparator.
     // Пример range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
-    public static List range(List list, Object min, Object max, Comparator comparator) {
-        throw new NotImplementedException();
+    public static <T> List<T> range(List<? extends T> list, T min, T max, Comparator<T> comparator) {
+        List<T> res = newArrayList();
+        for (T o: list) {
+            if (comparator.compare(o, min) >= 0 && comparator.compare(o, max) <= 0) {
+                res.add(o);
+            }
+        }
+        return res;
     }
 
 }
--- JavaSchoolLessons/Lesson4Generics/src/com/company/Main.java
+++ JavaSchoolLessons/Lesson4Generics/src/com/company/Main.java
@@ -9,11 +9,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        // пример использования CollectionUtil
+        testCollectionUtil();
+
         // пример использования CountMap
+        //testCountMap();
+    }
+
+    private static void testCountMap() {
         CountMap<Integer> map = new CountMapIml<>();
 
-        ArrayList<Integer> integers = new ArrayList<>();
-        integers.toArray();
         map.add(10);
         map.add(10);
         map.add(10);
@@ -35,5 +41,35 @@ public class Main {
         Map<Number, Integer> dest = new HashMap<>();
         map1.toMap(dest);
         dest.forEach((k, v) -> System.out.println(k + " " + v));
+    }
+
+    private static void testCollectionUtil() {
+        List<Integer> list = CollectionUtils.newArrayList();
+        CollectionUtils.add(list, 8);
+        CollectionUtils.add(list, 1);
+        CollectionUtils.add(list, 3);
+        CollectionUtils.addAll(Arrays.asList(5,6,4), list);
+        System.out.println("After addAll:");
+        list.forEach(l -> System.out.print(l + " "));
+        System.out.println();
+
+        CollectionUtils.removeAll(list, Arrays.asList(5,6,4));
+        System.out.println("After removeAll:");
+        list.forEach(l -> System.out.print(l + " "));
+        System.out.println();
+
+        boolean containsAll = CollectionUtils.containsAll(list, Arrays.asList(3,8,1));
+        System.out.println("containsAll = " + containsAll);
+
+        boolean containsAny = CollectionUtils.containsAny(list, Arrays.asList(10,16,3));
+        System.out.println("containsAny = " + containsAny);
+
+        list = CollectionUtils.range(Arrays.asList(8,1,3,5,6,4), 6, 8);
+        list.forEach(l -> System.out.print(l + " "));
+        System.out.println();
+
+        list = CollectionUtils.range(Arrays.asList(8,1,3,5,6,4), 3, 6, ((o1, o2) -> o1 - o2));
+        list.forEach(l -> System.out.print(l + " "));
+        System.out.println();
     }
 }
