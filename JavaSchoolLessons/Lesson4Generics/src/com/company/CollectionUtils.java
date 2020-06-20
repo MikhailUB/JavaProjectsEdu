2c19aa689a4a5a4c62c332cb2985cb78fd878744
Mikhail Bolotov <bolotovmu@mail.ru>
2020-06-20 17:16:47 +0300

    Задания лекции 4 Generics часть 2 CollectionUtil улучшение

--- JavaSchoolLessons/Lesson4Generics/src/com/company/CollectionUtils.java
+++ JavaSchoolLessons/Lesson4Generics/src/com/company/CollectionUtils.java
@@ -50,10 +50,10 @@ public class CollectionUtils {
     // Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
     // Элементы сравнивать через Comparable.
     // Пример range(Arrays.asList(8,1,3,5,6,4), 3, 6) вернет {3,4,5,6}
-    public static <T> List<T> range(List<? extends T> list, T min, T max) {
+    public static <T extends Comparable<? super T>> List<T> range(List<? extends T> list, T min, T max) {
         List<T> res = newArrayList();
         for (T o: list) {
-            Comparable<T> oComp = (Comparable<T>)o;
+            Comparable<? super T> oComp = o;
             if (oComp.compareTo(min) >= 0 && oComp.compareTo(max) <= 0) {
                 res.add(o);
             }
