1ad35b327b575bb762dffc9f6411456c734b9197
Mikhail Bolotov <bolotovmu@mail.ru>
2020-07-05 23:47:13 +0300

    Задания лекции 9 Lambda,Stream API контроль исходной коллекции

--- JavaSchoolLessons/Lesson9LambdaStreamAPI/src/ru/Mikhail/Main.java
+++ JavaSchoolLessons/Lesson9LambdaStreamAPI/src/ru/Mikhail/Main.java
@@ -12,12 +12,14 @@ public class Main {
         someCollection.add(new Person("Andrey", 25));
         someCollection.add(new Person("Marina", 47));
 
-        Map<?,?> m = Streams
+        Map<String,Person> m = Streams
                 .of(someCollection)
                 .filter(p -> p.getAge() > 20)
                 .transform(p -> new Person(p.getName(), p.getAge() + 30))
                 .toMap(p -> p.getName(), p -> p);
 
         m.forEach((k, v) -> System.out.println("key: " + k + " value: " + v));
+
+        someCollection.forEach(System.out::println);
     }
 }
